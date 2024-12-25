1b09c9a69c4d442cff09c4ad33630baffde2aef0
Awoke <awoke8817@gmail.com>
2024-12-25 16:26:28 +0300

    frontend env file edit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ async function connectDB() {
     try { 
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true, 
-            useUnifiedTopology: true,
+            useUnifiedTopology: true, 
         });    
         console.log("Connected to MongoDB");
     } catch (err) {
